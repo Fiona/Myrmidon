1028caaf7934c1519ba4417a32f39362cae4dc62
Fiona Burrows <fiona@justfiona.com>
2010-07-23 23:04:50 +0100

    Converted tabs to spaces.

--- backend_gfx_opengl.py
+++ backend_gfx_opengl.py
@@ -528,22 +528,22 @@ class MyrmidonGfxOpengl(object):
 			
 
 def frange(start, end=None, inc=None):
-    "A range function, that does accept float increments..."
+	"A range function, that does accept float increments..."
 
-    if end == None:
-        end = start + 0.0
-        start = 0.0
+	if end == None:
+		end = start + 0.0
+		start = 0.0
 
-    if inc == None:
-        inc = 1.0
+	if inc == None:
+		inc = 1.0
 
-    L = []
-    while 1:
-        next = start + len(L) * inc
-        if inc > 0 and next >= end:
-            break
-        elif inc < 0 and next <= end:
-            break
-        L.append(next)
+	L = []
+	while 1:
+		next = start + len(L) * inc
+		if inc > 0 and next >= end:
+			break
+		elif inc < 0 and next <= end:
+			break
+		L.append(next)
         
-    return L
+	return L
