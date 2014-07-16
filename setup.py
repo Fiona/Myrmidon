9464a18efd3790ba974bc60cc8c457f82eeb8125
Fiona Burrows <fiona@justfiona.com>
2014-07-16 13:26:58 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     url = 'http://www.github.com/Fiona/Myrmidon',
     packages = [
         'myrmidon', 'myrmidon.engine',
-        'myrmidon.engine.audio', 'myrmidon.engine.audio.pygame',
-        'myrmidon.engine.window', 'myrmidon.engine.window.pygame',
-        'myrmidon.engine.input', 'myrmidon.engine.input.pygame',
-        'myrmidon.engine.gfx', 'myrmidon.engine.gfx.opengl', 'myrmidon.engine.gfx.modern_opengl',
+        'myrmidon.engine.audio', 'myrmidon.engine.audio.pygame', 'myrmidon.engine.audio.kivy',
+        'myrmidon.engine.window', 'myrmidon.engine.window.pygame', 'myrmidon.engine.window.kivy',
+        'myrmidon.engine.input', 'myrmidon.engine.input.pygame', 'myrmidon.engine.input.kivy',
+        'myrmidon.engine.gfx', 'myrmidon.engine.gfx.opengl', 'myrmidon.engine.gfx.modern_opengl', 'myrmidon.engine.gfx.kivy',
     ],
     requires = ['pygame', 'OpenGL']
     )
