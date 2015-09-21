233cbbf60309f91afe26f0d1c24c1d1e3eb7f48e
Chozabu <chozabu@gmail.com>
2015-09-21 11:27:19 +0100

    faster circ-circ collisions by checking squared distance to avoid sqrt

--- myrmidon/Game.py
+++ myrmidon/Game.py
@@ -682,6 +682,10 @@ class Game(object):
     @classmethod
     def get_distance(cls, pointa, pointb):
         return math.sqrt((math.pow((pointb[1] - pointa[1]), 2) + math.pow((pointb[0] - pointa[0]), 2)))
+	
+    @classmethod
+    def get_distance2(cls, pointa, pointb):
+        return (math.pow((pointb[1] - pointa[1]), 2) + math.pow((pointb[0] - pointa[0]), 2))
 
     @classmethod
     def move_forward(cls, pos, distance, angle):
@@ -961,7 +965,7 @@ class Game(object):
         check_object_b_radius = check_object_b.collision_circle_calculate_radius()
 
         # Outside of each others radius
-        if check_object_a.get_distance((check_object_b.x, check_object_b.y)) > check_object_a_radius + check_object_b_radius:
+        if check_object_a.get_distance2((check_object_b.x, check_object_b.y)) > (check_object_a_radius + check_object_b_radius)**2:
             return False
 
         return True
