acfd417deff34fb789eea949211b904167cc39a9
Chozabu <chozabu@gmail.com>
2015-09-22 03:31:38 +0100

    implemented cymunk demo

--- examples/cymunk_balls.py
+++ examples/cymunk_balls.py
@@ -8,9 +8,20 @@ from myrmidon import Game, Entity
 from myrmidon.consts import *
 from pygame.locals import *
 import math, random
-import cymunk
+from cymunk import *
 
 shots = []
+# create the main space
+space = Space()
+#space.set_default_collision_handler(begin=mycollide)
+space.iterations = 30
+space.gravity = (0, 100)
+space.sleep_time_threshold = 0.5
+space.collision_slop = 0.5
+
+
+
+
 
 class Window(Entity):
 
@@ -35,33 +46,42 @@ class Window(Entity):
             text = "Hold space to produce more entities",
             alignment = ALIGN_CENTRE
             )
+        # add bounds
+        sx = Game.screen_resolution[0]
+        sy = Game.screen_resolution[1]
+        seg1 = Segment(space.static_body, Vec2d(0, 0), Vec2d(0, sy), 0)
+        seg1.elasticity = 1.0
+        seg1.friction = 1.0
+
+        seg2 = Segment(space.static_body, Vec2d(0, sy), Vec2d(sx, sy), 0)
+        seg2.elasticity = 1.0
+        seg2.friction = 1.0
+
+        seg3 = Segment(space.static_body, Vec2d(sx, sx), Vec2d(sx, 0), 0)
+        seg3.elasticity = 1.0
+        seg3.friction = 1.0
+
+        # add everything into space
+        space.add_static(seg1, seg2, seg3)
         while True:            
             if Game.keyboard_key_down(K_ESCAPE):
                 sys.exit()
-            if random.random()>.9:
-                self.pattern_vortex(300.0, 300.0, 1)
+            space.step(.1)
+            print space
+            #if random.random()>.9:
+            self.pattern_vortex(300.0, 300.0, 1)
             self.fps_text.text = "FPS " + str(Game.current_fps)
             self.entity_text.text = str(len(Game.entity_list)) + " entities"            
             yield
            
     def pattern_vortex(self, x, y, type = 0):
-        _range = 1
-        amount = 10.0
+        _range = 0
         if Game.keyboard_key_down(K_SPACE):
             _range = 10
-            amount = 5.0
+        if random.random()>.9:
+            _range=1
         for c in range(_range):
-            if type:
-                self.cur2 -= amount
-            else:
-                self.cur += amount           
-            if type:
-                if self.cur2 < -360.0:
-                    self.cur2 = 0.0
-            else:
-                if self.cur > 360.0:
-                    self.cur = 0.0
-            Shot(self, x, y, self.cur2 if type else self.cur)
+            Shot(self, x, y)
 
 
 
@@ -70,55 +90,30 @@ class Shot(Entity):
         self.image = game.graphics['shot']
         self.x = x
         self.y = y
-        self.ox = x+random.random()
-        self.oy = y+random.random()
         self.z = 512
+        self.body = Body(100, 1e9)
+        self.body.position = (x+random.random()*10.,y)
+        self.circle = Circle(self.body, self.image.width/2)
+        self.circle.elasticity = .10
+        self.circle.friction = .50
         self.collision_circle_radius = self.image.width/2
+        space.add(self.circle, self.body)
         self.age=0
         while True:
             self.age+=1
-            xv = self.x-self.ox
-            yv = self.y-self.oy
-            self.ox = self.x
-            self.oy = self.y
-            self.x+=xv*.98
-            self.y+=yv*.98+.1
-            shots = Game.get_entities(Shot)
-            slen = len(shots)
-            for ai in range(slen-1):
-                a=shots[ai]
-                for bi in range(ai+1, slen):
-                    b=shots[bi]
-                    dvx = a.x-b.x
-                    dvy = a.y-b.y
-                    dist_sqr = dvx**2+dvy**2
-                    rads = a.collision_circle_radius+b.collision_circle_radius
-                    #if Game.collision_circle_to_circle(a,b):
-                    if dist_sqr < rads**2:
-                        td = math.sqrt(dist_sqr)
-                        if td==0:td=0.0001
-                        nx=dvx/td
-                        ny=dvy/td
-                        diffd = td-rads
-                        fx = nx*diffd
-                        fy = ny*diffd
-                        a.x -= fx*.5
-                        a.y -= fy*.5
-                        b.x += fx*.5
-                        b.y += fy*.5
-                        #b.x -= fx
-                        #b.y -= fy
-            #if self.x < 0 or self.x > Game.screen_resolution[0]-0 or self.y < 0 or self.y > Game.screen_resolution[1]:
-            if self.y > Game.screen_resolution[1]*.7:
+            self.x=self.body.position.x
+            self.y=self.body.position.y
+            #print self.body.position
+            '''if self.y > Game.screen_resolution[1]*.7:
                 self.y=Game.screen_resolution[1]*.7
             if self.x > Game.screen_resolution[0]*.4:
                 self.x=Game.screen_resolution[0]*.4
             if self.x < 20:
-                self.x=20
-            if self.age>600:
-                self.destroy()
+                self.x=20'''
+            #if self.age>60:
+            #    self.destroy()#TODO remove/destroy body/shape
             yield
-                        
+
 
 # Start game
 Game.screen_resolution = (1024, 768)
